9722d97156e05599fbf79d408aaf3f16b7ff4d3c
Stas Sribnyi <stas.sribnyi@gmail.com>
2023-10-20 19:10:55 +0300

    Init vite app

--- src/js/ActionConstants.js
+++
@@ -1,4 +0,0 @@
-export default {
-    SET_RANDOM_NAME: 'SET_RANDOM_NAME',
-    SET_RANDOM_INFO: 'SET_RANDOM_INFO'
-}
--- src/js/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import userReducer from './userReducer';
-import infoReducer from './infoReducer';
-
-export default combineReducers({
-  user: userReducer,
-  info: infoReducer,
-});
