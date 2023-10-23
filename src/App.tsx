b82626045224b1666f648e6dc6dee3017f28d93b
Stas Sribnyi <stas.sribnyi@gmail.com>
2023-10-24 01:36:52 +0300

    Update UI using picocss

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import { WeatherInfo } from './vite-env';
 
 import './App.css';
 import { getWMOInfo, getWMOInfoDaily, getWMOInfoHourly } from './getWMOInfo';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
+  faLocationDot,
   faMagnifyingGlass,
   faWater,
   faWind,
 } from '@fortawesome/free-solid-svg-icons';
 import { addHours, compareAsc, format, isToday, subHours } from 'date-fns';
+import { useDraggable } from 'react-use-draggable-scroll';
 
 const DETAILS_QUERY = {
   latitude: 50.4547,
@@ -107,58 +109,62 @@ function getDailyInfo(weatherInfo: WeatherInfo) {
 const DailyReport: FC<Readonly<{ weatherInfo: WeatherInfo }>> = ({
   weatherInfo,
 }) => (
-  <details>
-    <summary style={{ cursor: 'pointer' }}>7 Days Weather Report</summary>
-    <ul style={{ listStyle: 'none', margin: '1rem 0 0', padding: 0 }}>
-      {getDailyInfo(weatherInfo).map(
-        ({ time, description, imageUrl, temperature }, idx) => (
-          <li key={idx}>
-            <div
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                gap: '0.75rem',
-                fontSize: '0.75rem',
-                // justifyContent: 'space-between',
-              }}
-            >
-              <p style={{ margin: 0 }}>{format(time, 'LLL d')}</p>
-              <p style={{ margin: 0, width: '38px' }}>
-                {isToday(time) ? 'Today' : format(time, 'EEE')}
-              </p>
-              <img src={imageUrl} style={{ width: '38px', height: '38px' }} />
-              <p style={{ margin: 0, width: '60px' }}>{description}</p>
-              <p style={{ margin: 0 }}>
-                {temperature.max}
-                {temperature.units}/{temperature.min}
-                {temperature.units}
-              </p>
-            </div>
-          </li>
-        )
-      )}
-    </ul>
-  </details>
+  <table>
+    {getDailyInfo(weatherInfo).map(
+      ({ time, description, imageUrl, temperature }, idx) => (
+        <tr key={idx}>
+          <td style={{ margin: 0 }}>{format(time, 'LLL d')}</td>
+          <td style={{ margin: 0, width: '38px' }}>
+            {isToday(time) ? 'Today' : format(time, 'EEE')}
+          </td>
+          <img src={imageUrl} style={{ minWidth: '38px', width: '38px' }} />
+          <td style={{ margin: 0, width: '60px' }}>{description}</td>
+          <td style={{ margin: 0 }}>
+            {temperature.max}
+            {temperature.units}/{temperature.min}
+            {temperature.units}
+          </td>
+        </tr>
+      )
+    )}
+  </table>
 );
 
 const HourlyReport: FC<Readonly<{ weatherInfo: WeatherInfo }>> = ({
   weatherInfo,
-}) => (
-  <ul className='horizontal-scroll '>
-    {getHourlyInfo(weatherInfo).map(({ time, imageUrl, temperature }, idx) => (
-      <li key={idx}>
-        <div>
-          <p style={{ margin: 0 }}>{format(time, 'HH:mm')}</p>
-          <img src={imageUrl} style={{ width: '40px', height: '40px' }} />
-          <p style={{ margin: 0 }}>
-            {temperature.value}
-            {temperature.units}
-          </p>
-        </div>
-      </li>
-    ))}
-  </ul>
-);
+}) => {
+  const ref = useRef<HTMLElement>();
+  const { events } = useDraggable(ref);
+
+  return (
+    <figure  {...events} ref={ref} style={{
+      userSelect: 'none'
+    }}>
+      <table>
+        <tr>
+          {getHourlyInfo(weatherInfo).map(
+            ({ time, imageUrl, temperature }, idx) => (
+              <td key={idx}>
+                <div>
+                  <p style={{ margin: 0 }}>{format(time, 'HH:mm')}</p>
+                  <img
+                    draggable={false}
+                    src={imageUrl}
+                    style={{ width: '40px', height: '40px' }}
+                  />
+                  <p style={{ margin: 0 }}>
+                    {temperature.value}
+                    {temperature.units}
+                  </p>
+                </div>
+              </td>
+            )
+          )}
+        </tr>
+      </table>
+    </figure>
+  );
+};
 
 const CurrentReport: FC<Readonly<{ weatherInfo: WeatherInfo }>> = ({
   weatherInfo,
@@ -190,75 +196,41 @@ const CurrentReport: FC<Readonly<{ weatherInfo: WeatherInfo }>> = ({
     <>
       <div
         style={{
-          display: 'flex',
-          justifyContent: 'space-between',
+          display: 'grid',
+          gridTemplate: '1fr 1fr / 3fr minmax(100px, 1fr)',
+          gap: '2rem',
         }}
       >
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
+        <div>
           <p style={{ fontSize: '3.5rem', margin: 0 }}>
             {current.temperature.value}
             {current.temperature.units}
           </p>
-          <p style={{ fontSize: '0.85rem', margin: 0 }}>
+          <p style={{ margin: 0 }}>
             Feels like {current.apparentTemperature.value}
             {current.apparentTemperature.units}
           </p>
         </div>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
+        <div style={{ justifySelf: 'end' }}>
           <img
             src={current.imageUrl}
             style={{ width: '84px', height: '84px' }}
           />
-          <p style={{ fontSize: '0.85rem', margin: 0 }}>
-            {current.description}
-          </p>
+          <p style={{ margin: 0 }}>{current.description}</p>
         </div>
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '0.5rem',
-            alignItems: 'center',
-          }}
-        >
+        <div>
           <p style={{ fontSize: '1.5rem', margin: 0 }}>
             <FontAwesomeIcon icon={faWind} /> {current.windspeed.value}
             {current.windspeed.units}
           </p>
-          <p style={{ fontSize: '0.85rem', margin: 0 }}>Wind speed</p>
+          <p style={{ margin: 0 }}>Wind speed</p>
         </div>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '0.5rem',
-            alignItems: 'center',
-          }}
-        >
+        <div style={{ justifySelf: 'end' }}>
           <p style={{ fontSize: '1.5rem', margin: 0 }}>
             <FontAwesomeIcon icon={faWater} /> {current.relativeHumidity.value}
             {current.relativeHumidity.units}
           </p>
-          <p style={{ fontSize: '0.85rem', margin: 0 }}>Humidity</p>
+          <p style={{ margin: 0 }}>Humidity</p>
         </div>
       </div>
     </>
@@ -281,7 +253,13 @@ const LocationForm: FC<
     );
     fetch(url.toString())
       .then((resp) => resp.json())
-      .then((res) => setOptions(res.results || []))
+      .then((res) =>
+        setOptions(
+          ((res.results || []) as Array<Option>).filter(
+            (x) => x.latitude && x.longitude
+          )
+        )
+      )
       .catch(console.log);
   }, [search]);
 
@@ -298,13 +276,7 @@ const LocationForm: FC<
   };
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        gap: '2rem',
-        marginBottom: '1rem',
-      }}
-    >
+    <div>
       <input
         value={search}
         style={{ width: '100%' }}
@@ -343,7 +315,7 @@ async function getForecast(
 
   params.set('latitude', `${city.latitude}`);
   params.set('longitude', `${city.longitude}`);
-  params.set('timezone', city.timezone);
+  params.set('timezone', city.timezone || DETAILS_QUERY.timezone);
   params.set('past_days', '1');
 
   return fetch(url.toString()).then((resp) => resp.json());
@@ -351,6 +323,7 @@ async function getForecast(
 
 function App() {
   const [weatherInfo, setWeatherInfo] = useState<null | WeatherInfo>(null);
+  const [isModalOpen, setOpenModal] = useState(false);
   const [city, setCity] = useState<
     Pick<Option, 'latitude' | 'longitude' | 'name' | 'timezone'>
   >({
@@ -366,54 +339,66 @@ function App() {
       .catch(console.log);
   }, [city]);
 
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      'data-theme',
+      weatherInfo?.current.is_day ? 'light' : 'dark'
+    );
+  }, [weatherInfo]);
+
   return (
     <>
-      <div
+      <button
         style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '2rem',
-          alignItems: 'start',
+          position: 'absolute',
+          top: '8px',
+          left: '8px',
+          width: '56px',
         }}
+        onClick={() => setOpenModal(true)}
       >
+        <FontAwesomeIcon icon={faLocationDot} />
+      </button>
+      <div>
+        <h1 style={{ margin: '0 0 16px 0', textAlign: 'center' }}>
+          Weather Forecast
+        </h1>
         {weatherInfo && (
-          <div
-            style={{
-              boxSizing: 'border-box',
-              width: '100%',
-              padding: '2rem',
-              display: 'flex',
-              flexDirection: 'column',
-              gap: '1.5rem',
-              color: '#346284e8',
-              border: '2px solid #346284e8',
-
-              ...(!weatherInfo?.current.is_day && {
-                color: 'white',
-                backgroundColor: '#346284e8',
-              }),
-            }}
-          >
-            <LocationForm handleSubmit={setCity} />
-            <p style={{ fontSize: '2rem', margin: 0, fontWeight: 700 }}>
-              {city?.name}
-            </p>
-            <CurrentReport weatherInfo={weatherInfo} />
-            <HourlyReport weatherInfo={weatherInfo} />
+          <article className='grid'>
+            <div>
+              <h2 style={{ fontSize: '2rem', margin: 0, fontWeight: 700 }}>
+                {city?.name}
+              </h2>
+              <CurrentReport weatherInfo={weatherInfo} />
+              <HourlyReport weatherInfo={weatherInfo} />
+            </div>
             <DailyReport weatherInfo={weatherInfo} />
-          </div>
+          </article>
         )}
 
-        <details>
-          <summary>API response</summary>
-          <pre
-            className='prettyprint'
-            dangerouslySetInnerHTML={{
-              __html: JSON.stringify(weatherInfo, undefined, 2),
+        <article>
+          <details>
+            <summary>API response</summary>
+            <pre
+              className='prettyprint'
+              dangerouslySetInnerHTML={{
+                __html: JSON.stringify(weatherInfo, undefined, 2),
+              }}
+            />
+          </details>
+        </article>
+      </div>
+      <dialog open={isModalOpen} onClose={() => setOpenModal(false)}>
+        <article>
+          <h3>Looking for your city?</h3>
+          <LocationForm
+            handleSubmit={(city) => {
+              setCity(city);
+              setOpenModal(false);
             }}
           />
-        </details>
-      </div>
+        </article>
+      </dialog>
     </>
   );
 }
