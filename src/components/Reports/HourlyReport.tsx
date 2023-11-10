03b750a8bd73ef7306b3d88fb3e110b10ce929a5
Stas Sribnyi <stas.sribnyi@gmail.com>
2023-11-10 22:31:40 +0200

    Simplify hours report

--- src/components/Reports/HourlyReport.tsx
+++ src/components/Reports/HourlyReport.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Table, TableBody, TableRow, TableCell, TableContainer, Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 import { Precipitation } from '../Precipitation';
 import { Temperature } from '../Temperature';
@@ -10,29 +10,19 @@ import type { HourlyForecast } from '../../mappers';
 
 export const HourlyReport: FC<Readonly<{ value: Array<HourlyForecast> }>> = ({ value }) => {
   return (
-    <TableContainer sx={{ pb: '1rem' }}>
-      <Box sx={{ minWidth: 'fit-content' }}>
-        <Table size="small">
-          <TableBody>
-            <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              {value.map(({ time, isDay, precipitation, temperature, weathercode }, idx) => (
-                <TableCell key={idx} sx={{ p: 0.5 }}>
-                  <Box sx={{ display: 'grid', placeItems: 'center' }}>
-                    <strong style={{ fontSize: '1rem', margin: 0 }}>
-                      <Temperature value={temperature} />
-                    </strong>
-                    <WMOIcon variant={isDay ? 'day' : 'night'} code={weathercode} size={40} />
-                    <Box sx={{ margin: 0, fontSize: '0.75rem', mb: 1 }}>
-                      <Time value={time} format="HH:mm" />
-                    </Box>
-                    <Precipitation showLabel level={precipitation} size={12} />
-                  </Box>
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableBody>
-        </Table>
-      </Box>
-    </TableContainer>
+    <Box component="ul" sx={{ display: 'flex', listStyle: 'none', overflowX: 'auto', m: 0, p: 0, pb: '1rem' }}>
+      {value.map(({ time, isDay, precipitation, temperature, weathercode }, idx) => (
+        <Box component="li" key={idx} sx={{ display: 'grid', placeItems: 'center', p: 0.5 }}>
+          <Typography sx={{ fontSize: '1rem', fontWeight: 700, m: 0 }}>
+            <Temperature value={temperature} />
+          </Typography>
+          <WMOIcon variant={isDay ? 'day' : 'night'} code={weathercode} size={40} />
+          <Typography sx={{ fontSize: '0.75rem', m: 0, mb: 1 }}>
+            <Time value={time} format="HH:mm" />
+          </Typography>
+          <Precipitation showLabel level={precipitation} size={12} />
+        </Box>
+      ))}
+    </Box>
   );
 };
