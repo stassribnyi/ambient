d18d4e5e2c27868498a383e589f3d7857f34f539
Stas Sribnyi <stas.sribnyi@gmail.com>
2024-03-29 03:52:50 +0200

    Add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,66 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import path from 'path';
+
+import react from '@vitejs/plugin-react';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: 'prompt',
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+  manifest: {
+    name: 'Ambient',
+    short_name: 'Ambient',
+    description: 'It is simple and user-friendly page with up-to-date weather forecasts for your favorite locations.',
+    icons: [
+      {
+        src: '/manifest-icon-192.maskable.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: '/manifest-icon-192.maskable.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/manifest-icon-512.maskable.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: '/manifest-icon-512.maskable.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/apple-touch-icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+        purpose: 'apple touch icon',
+      },
+      {
+        src: '/maskable_icon.png',
+        sizes: '225x225',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+    theme_color: 'rgb(171, 71, 188)',
+    background_color: 'rgba(17, 25, 31, 0.85)',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait',
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
   base: '/ambient/',
   resolve: {
     alias: {
