ce5470dd9c069a954f93cbae1758b7e52bdd7535
Stas Sribnyi <stas.sribnyi@gmail.com>
2024-03-29 21:43:04 +0200

    Meet requirements for PWA install

--- vite.config.ts
+++ vite.config.ts
@@ -6,40 +6,61 @@ import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
 const manifestForPlugin: Partial<VitePWAOptions> = {
   registerType: 'prompt',
-  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'maskable-icon.png'],
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
   manifest: {
     name: 'Ambient',
     short_name: 'Ambient',
     description: 'It is simple and user-friendly page with up-to-date weather forecasts for your favorite locations.',
     icons: [
       {
-        src: '/android-chrome-192x192.png',
+        src: 'android-chrome-192x192.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/android-chrome-512x512.png',
+        src: 'android-chrome-512x512.png',
         sizes: '512x512',
         type: 'image/png',
       },
       {
-        src: '/apple-touch-icon.png',
-        sizes: '180x180',
+        src: 'android-chrome-144x144.png',
+        sizes: '144x144',
         type: 'image/png',
-        purpose: 'apple touch icon',
       },
       {
-        src: '/maskable-icon.png',
+        src: 'maskable-icon.png',
         sizes: '225x225',
         type: 'image/png',
-        purpose: 'any maskable',
+        purpose: 'any',
+      },
+      {
+        src: 'maskable-icon.png',
+        sizes: '225x225',
+        type: 'image/png',
+        purpose: 'maskable',
       },
     ],
-    theme_color: '#ab47bc',
+    screenshots: [
+      {
+        src: 'screenshot-wide.png',
+        sizes: '650x480',
+        type: 'image/png',
+        form_factor: 'wide',
+        label: 'Ambient Desktop',
+      },
+      {
+        src: 'screenshot-narrow.png',
+        sizes: '450x925',
+        type: 'image/png',
+        form_factor: 'narrow',
+        label: 'Ambient Mobile',
+      },
+    ],
+    theme_color: '#11191f',
     background_color: '#11191f',
     display: 'standalone',
-    scope: '/',
-    start_url: '/',
+    scope: '/ambient/',
+    start_url: '/ambient/',
     orientation: 'portrait',
   },
 };
