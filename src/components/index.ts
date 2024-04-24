ceea654858dd9007d4d14f94ea7c26ce97543d62
Stas Sribnyi <stas.sribnyi@gmail.com>
2024-04-25 00:16:42 +0300

    Code clean up

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,6 @@ export { Block } from './Block';
 export { Fallback } from './Fallback';
 export { Layout } from './Layout';
 export { MenuDialog } from './Menu';
-export { Time } from './Time';
 export { UnitSwitch } from './UnitSwitch';
 export { Temperature } from './Temperature';
 export { Branding } from './Branding';
